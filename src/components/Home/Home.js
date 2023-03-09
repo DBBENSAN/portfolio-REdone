88b74738f5d4d5cfeeaef98d4ecbff18c3377984
DBENSAN <dftb97@gmail.com>
2023-03-08 23:18:00 -0800

    Adds home page w content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
 import React, { useState } from "react";
+import Carousel from './Carousel'
 
 export default function Home() {
-   return(
-      
+   return (
+      <div class="hero">
+         <article>
+            <div class="sentence">
+               <p>Hi--</p><p><span id="f-name">I'm Dan</span>iele Bensan</p>
+            </div>
+            <div class="sentence">
+               <p id="f-title">Full Stack Developer</p>
+            </div>
+            <div class="sentence">
+               <p>Aspiring Photographer</p>
+            </div>
+            <div class="sentence">
+               <p>See my work,</p><p id="f-words">follow my journey</p>
+            </div>
+         </article>
+         <Carousel />
+      </div>
    )
 }
